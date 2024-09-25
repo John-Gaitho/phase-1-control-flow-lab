88a5999bf86d52d7d951138f89f0b28a234626c9
John Gaitho <jgaitho016@gmail.com>
2024-09-25 14:54:35 +0300

    completed assingment

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <=400) { return "This one is on me!"}
+  else if (400<= feet && feet <= 2000){ return "That will be twenty bucks.";
 }
-
-function ternaryCheckCity(){
-  // Write your code here!
+  else if (2000<= feet && feet <=2500){ return "I will gladly take your thirty bucks.";
 }
+  else { return "No can do."};
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+}
+function ternaryCheckCity(city){
+return city === "NYC"?"Ok, sounds good.":"No go."
+
+}
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case tip = "generous":
+    return "Thank you so much."
+    break;
+    case tip= "not as generous":
+      return "Thank you."
+      break;
+      default:
+        return "Bye.";
+
+  }
 }
